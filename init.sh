591ba55ff1218abc23926a28d92789820f9dfd94
lambert <lambert_Y_Y@163.com>
2022-01-07 18:01:34 +0800

    create init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "这是一个初始化 Spring Boot 项目脚本，可以生成目录及目录下固定的java文件：\n /controller/dao/dto/entity/service.serviceImpl/主类/"
+
+mkdir -pv ./src/{main/java/com/Lambert/fun/{config,controller,dao,dto,entity,service/serviceImpl,config,exception,result,utils},resource}
